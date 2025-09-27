d2223eb462733370f37233c461394cb85ee27900
JaySmith043 <jayds0403@gmail.com>
2025-09-26 22:05:45 -0400

    Add files via upload

---
+++ Step_1.py
@@ -0,0 +1,17 @@
+# [jedewittsmith]_assignment_3.py
+# COMP 163: Assignment 4 - College Life Adventure Game
+
+
+student_name = "Jadyn"
+current_gpa = 3.9
+study_hours = 25
+social_points = 50
+stress_level = 80
+
+
+print("=== Welcome to the College Life Adventure Game! ===")
+print(f"Student: {student_name}")
+print(f"Current GPA: {current_gpa}")
+print(f"Study Hours: {study_hours}")
+print(f"Social Points: {social_points}")
+print(f"Stress Level: {stress_level}/100")
